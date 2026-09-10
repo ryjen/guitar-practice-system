84b9f72307f6ff4c7c6a6718deaeef6c3b2b511f
RJ <rybertjen@gmail.com>
2026-09-09 23:49:51 -0700

    refactor: add application layer

---
+++ guitar_practice/application/__init__.py
@@ -0,0 +1,5 @@
+"""Application use cases and ports.
+
+This layer coordinates domain behavior. It does not know about argparse, concrete
+filesystems, model providers, or presentation concerns.
+"""
